3d506d32041463f0a1f1fcaef6ac6613226746d0
Jack <jack@codeplay.com>
2019-02-07 09:14:30 +0000

    Edited prompt file

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -2,9 +2,51 @@
 # Widgets for displaying mode when using vim mode.
 autoload -Uz promptinit && promptinit
 
+# Prompt string is subject to expansion before it is drawn.
 setopt prompt_subst
 
+# For color formatting.
 autoload -Uz colors && colors
+
+# Useful strings to put in prompt.
+CYAN="%B%F{cyan}"
+RED="%B%F{red}"
+YELLOW="%B%F{yellow}"
+WHITE="%B%F{white}"
+GREEN="%B%F{green}"
+GREY="%B%F{grey}"
+# This is the color that indicates whether in VI mode.
+CURRENT_COLOR=${WHITE}
+RESET_COLOR="%b%f"
+
+OPEN_BRA="${CYAN}[% ${RESET_COLOR}"
+CLOSE_BRA="${CYAN}]% ${RESET_COLOR}"
+
+USER_NAME="${RED}%n${RESET_COLOR}"
+WORK_DIR="${GREEN}%1d${RESET_COLOR}"
+# This value changes color based on the current mode.
+PRIVLIGE='${CURRENT_COLOR}%#${RESET_COLOR}'
+EXIT_STATUS="%(?..${OPEN_BRA}${RED}%?${RESET_COLOR}${CLOSE_BRA})"
+PROMPT_END="${WHITE}: ${RESET_COLOR}"
+
+# For VC options.
+autoload -Uz vcs_info
+# Enable this for git only.
+zstyle ':vcs_info:*' enable git
+# Just display the branch name in the prompt.
+zstyle ':vcs_info:git*' formats  "%B%F{yellow}%b%f"
+# This function gets called before each prompt.
+# It is not executed simplt because the command line is redrawn.
+precmd() {
+    vcs_info
+}
+
+BRANCH='${GREY}${vcs_info_msg_0_}$RESET_COLOR'
+
+# The Prompt.
+PS1=${OPEN_BRA}${USER_NAME}${CLOSE_BRA}${OPEN_BRA}${WORK_DIR}${CLOSE_BRA}${EXIT_STATUS}${OPEN_BRA}${PRIVLIGE}${CLOSE_BRA}${PROMPT_END}
+# Remove any RS prompt.
+RPS1=
 
 # Function to change color of variables based on whether in normal 
 # or insert mode.
@@ -17,17 +59,6 @@ function _zle_line_init _zle_keymap_select {
         NORMAL_COLOR="%B%F{yellow}"
         # Set current color based on mode.
         CURRENT_COLOR="${${KEYMAP/vicmd/${NORMAL_COLOR}}/(main|viins)/${INSERT_COLOR}}"
-
-        RESET_COLOR="%b%f"
-        OPEN_BRA="%{$fg_bold[cyan]%}[%{$reset_color%}"
-        CLOSE_BRA="%{$fg_bold[cyan]%}]%{$reset_color%}"
-        USER_NAME="%B%F{red}%n%b%f"
-        WORK_DIR="%{$fg_bold[green]%}%1d%{$reset_color%}"
-        PRIVLIGE="${CURRENT_COLOR}%#${RESET_COLOR}"
-        EXIT_STATUS="%(?..${OPEN_BRA}%B%F{red}%?%b%f${CLOSE_BRA})"
-        PROMPT_END="%B%F{white}: %b%f"
-
-        PS1="${OPEN_BRA}${USER_NAME}${CLOSE_BRA}${OPEN_BRA}${WORK_DIR}${CLOSE_BRA}${EXIT_STATUS}${OPEN_BRA}${PRIVLIGE}${CLOSE_BRA}${PROMPT_END}"
         # Redraw the prompt when this function is finished.
         zle reset-prompt
 }
